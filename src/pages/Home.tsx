8a5135071c306fad464ed4e3b006f9855005e4e5
Eduardo Reche Lopes <eduardo.rechelopes@exxonmobil.com>
2021-08-24 20:22:49 -0300

    feat: add tasks to the list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,14 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks([
+      ...tasks,
+      {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false,
+      },
+    ]);
   }
 
   function handleToggleTaskDone(id: number) {
@@ -26,18 +33,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: '#EBEBEB',
+  },
+});
